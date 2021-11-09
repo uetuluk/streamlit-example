769949e8590b48a48d152f59b209eb04659890bd
Utku Ege Tuluk <36725861+uetuluk@users.noreply.github.com>
2021-11-09 14:01:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,28 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+import plotly.express as px
+import pandas as pd
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+st.header("hello world!")
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+medals = "./data/Country_Medals.csv"
 
-    points_per_turn = total_points / num_turns
+# ---------------------------
+# TASK 0: load in data
+@st.cache(persist = True)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+def load_data(data):
+    data = pd.read_csv(data)
+    return data
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+data = load_data(medals)
+# we need to cache the output of load_data if the input doesn't change
+# so that the CPU cycle is not jammed every time you move the slider/ re-run app in any way
+# essentially, we don't want to reload the data every time when we perform an interaction
+
+# at this point, you are not going to see anything after you rerun the app
+# because we haven't had any output yet
+
+# let's try to show the data here
+st.write(data)
+
